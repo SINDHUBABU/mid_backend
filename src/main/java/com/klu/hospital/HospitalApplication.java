3ec48627c2fb555773bfa5baf07a08ebcca4e15d
SINDHUBABU <sindhubabs2@gmail.com>
2025-10-14 15:44:36 +0530

    v2.0.0

--- src/main/java/com/klu/hospital/HospitalApplication.java
+++ src/main/java/com/klu/hospital/HospitalApplication.java
@@ -2,12 +2,19 @@ package com.klu.hospital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HospitalApplication {
+public class HospitalApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HospitalApplication.class, args);
-	}
+    // This method allows the application to be deployed as a WAR in Tomcat
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HospitalApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalApplication.class, args);
+    }
 }
